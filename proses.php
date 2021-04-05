f8e5532880b6e4b4a6ead0eeacaa52397d437f2d
junico17 <muhammad.junico17@gmail.com>
2021-04-04 21:03:42 -0700

    coding pertama

---
+++ proses.php
@@ -0,0 +1,13 @@
+<?php 
+$angka1=$_POST['angka1'];
+$angka2=$_POST['angka2'];
+
+$hasil=$angka1 + $angka2;
+
+
+
+ ?>
+
+
+ <h1> hasilnya adalah<?php echo $hasil ?></h1>;
+ <a href="index.php"><h1>hitung lagi</h1></a>;
